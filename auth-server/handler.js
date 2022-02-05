cd727bc4a478e7a7d932275a0699e8b4d955705c
Alexander Urlich <ulrichalex12@yahoo.de>
2022-02-06 00:48:26 +0100

    pt3

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -12,9 +12,9 @@ const credentials = {
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  redirect_uris: ['https://FluffyderDelphin.github.io/Meet_App/'],
+  redirect_uris: ['https://fluffyderdelphin.github.io/Meet_App/'],
   javascript_origins: [
-    'https://FluffyderDelphin.github.io',
+    'https://fluffyderdelphin.github.io',
     'http://localhost:3000',
   ],
 };
