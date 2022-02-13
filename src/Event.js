109332a1c95241723f608502cc47644f24c95b68
Alexander Urlich <ulrichalex12@yahoo.de>
2022-02-13 14:41:32 +0100

    implmented ShowDetails Test

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,28 @@
+import { shallow } from 'enzyme';
 import React, { Component } from 'react';
 
 class Event extends Component {
+  state = {
+    ShowDetails: false,
+  };
+  showDetailsButton = 'Show Details';
+  handleDetails = () => {
+    const showDetails = this.state.ShowDetails;
+    if (!showDetails) {
+      this.setState({ ShowDetails: true });
+      this.showDetailsButton = 'Hide Details';
+    } else {
+      this.setState({ ShowDetails: false });
+      this.showDetailsButton = 'Show Details';
+    }
+  };
   render() {
-    return <div></div>;
+    const { event } = this.props;
+    return (
+      <div>
+        <button className="showDetails">{this.showDetailsButton}</button>
+      </div>
+    );
   }
 }
 
---
+++ src/_tests_/Event.test.js
@@ -0,0 +1,26 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import { mockData } from '../mock-data';
+import Event from '../Event';
+
+describe('<Event /> components', () => {
+  let EventWrapper;
+  beforeAll(() => {
+    EventWrapper = shallow(<Event />);
+  });
+  test('"Show Details"Button will show more Details about Event', () => {
+    EventWrapper.setState({
+      ShowDetails: false,
+    });
+    EventWrapper.find('.showDetails').simulate('click');
+    expect(EventWrapper.state('ShowDetails')).toBe(true);
+  });
+
+  test('"Hide Details"Button will hide Details about the Event', () => {
+    EventWrapper.setState({
+      ShowDetails: true,
+    });
+    EventWrapper.find('.showDetails').simulate('click');
+    expect(EventWrapper.state('ShowDetails')).toBe(false);
+  });
+});
