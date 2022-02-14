7ff2dd9f038b68f5d67886238df97226c44d34b9
Alexander Urlich <ulrichalex12@yahoo.de>
2022-02-14 21:28:28 +0100

    corrected EventList Error

--- src/EventList.js
+++ src/EventList.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import Event from './Event';
 
 function Eventlist({ events, numberOfEvents }) {
-  let renderEventList = function (list, number) {
+  let renderEventList = (list, number) => {
     for (let i = 0; i < number; i++) {
-      return;
-      <li>
-        <Event event={list[1]} />
-      </li>;
+      return (
+        <li>
+          <Event event={list[1]} />
+        </li>
+      );
     }
   };
+
   return (
     <ul className="EventList">
       {/* {events.map((event) => (
