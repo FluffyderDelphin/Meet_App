af70e8864159e562f24c72037b23a29afe7aba3f
Alexander Urlich <ulrichalex12@yahoo.de>
2022-02-14 21:25:56 +0100

    corrected mistake wiht renderEventlistFunction

--- src/EventList.js
+++ src/EventList.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import Event from './Event';
 
 function Eventlist({ events, numberOfEvents }) {
-  let renderEventList = (list,number) =>{
-    return {for(let i= 0;i < number ;i++){
+  let renderEventList = function (list, number) {
+    for (let i = 0; i < number; i++) {
+      return;
       <li>
-      <Event event={list[1]} />
-      </li>
-    
-
-    }}
-
-  }
+        <Event event={list[1]} />
+      </li>;
+    }
+  };
   return (
     <ul className="EventList">
       {/* {events.map((event) => (
@@ -19,7 +17,7 @@ function Eventlist({ events, numberOfEvents }) {
           <Event event={event} />
         </li>
       ))} */}
-      {renderEventList(events,numberOfEvents);}
+      {renderEventList(events, numberOfEvents)}
     </ul>
   );
 }
