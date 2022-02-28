b91ea29e126fe2aaac22a22b09b27cc276b1ad9c
Alexander Urlich <ulrichalex12@yahoo.de>
2022-02-28 10:20:54 +0100

    wirting out test scenarios

--- src/_tests_/features/filterEventsByCity.test.js
+++
@@ -1,7 +0,0 @@
-import { loadFeature, defineFeature } from 'jest-cucumber';
-
-const feature = loadFeature(
-  './src/_tests_/features/filterEventsByCity.test.js'
-);
-
-defineFeature(feature, (test) => {});
