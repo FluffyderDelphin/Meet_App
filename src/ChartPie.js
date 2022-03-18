c186630ae04e788d068697d21c55d910aab3c980
Alexander Urlich <ulrichalex12@yahoo.de>
2022-03-18 21:38:42 +0100

    updated PieChart

--- src/ChartPie.js
+++ src/ChartPie.js
@@ -29,21 +29,23 @@ export default function ChartPie({ events }) {
   ];
 
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <PieChart width={400} height={400}>
-        <Pie
-          data={placeholderData}
-          cx={200}
-          cy={200}
-          labelLine={false}
-          label={({ name, percent }) =>
-            `${name} ${(percent * 100).toFixed(0)}%`
-          }
-          outerRadius={80}
-          fill="#8884d8"
-          dataKey="value"
-        ></Pie>
-      </PieChart>
-    </ResponsiveContainer>
+    <div style={{ height: '400px' }}>
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart width={400} height={400}>
+          <Pie
+            data={placeholderData}
+            cx={200}
+            cy={200}
+            labelLine={false}
+            label={({ name, percent }) =>
+              `${name} ${(percent * 100).toFixed(0)}%`
+            }
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          ></Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
