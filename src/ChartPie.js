7a53823995de6a77c3972f654309269081657af6
Alexander Urlich <ulrichalex12@yahoo.de>
2022-03-18 21:45:36 +0100

    changed chart to example one

--- src/ChartPie.js
+++ src/ChartPie.js
@@ -28,22 +28,56 @@ export default function ChartPie({ events }) {
     { name: 'Group D', value: 200 },
   ];
 
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
   return (
     <div style={{ height: '400px' }}>
       <ResponsiveContainer width="100%" height="100%">
         <PieChart width={400} height={400}>
           <Pie
-            data={placeholderData}
-            cx={200}
-            cy={200}
+            data={data}
+            cx="50%"
+            cy="50%"
             labelLine={false}
-            label={({ name, percent }) =>
-              `${name} ${(percent * 100).toFixed(0)}%`
-            }
+            label={renderCustomizedLabel}
             outerRadius={80}
             fill="#8884d8"
             dataKey="value"
-          ></Pie>
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
         </PieChart>
       </ResponsiveContainer>
     </div>
